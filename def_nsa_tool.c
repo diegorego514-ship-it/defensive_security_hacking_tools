f38ad31c6d1fc363ec3b713bc56cb064c7fd15db
diegorego514-ship-it <diegorego514@gmail.com>
2025-11-07 20:12:43 -0300

    Add files via upload

---
+++ def_nsa_tool.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Implementando auditioria de segurança de infraestruturas para redes locais e internas
+#define TARGET IPADDRESS 168.87.125.215
+
+/*Escopo de um alvo raiz
+tipo_de_retorno nome_do_alvo(parametros)
+{
+    comandos...
+}
+*/
+//Função localizar possíveis alvos
+int extração ; enum dados(enum ext1, ext2); //Extraí dados de alvos conectados através de uma backdoor ou conexão interna/local e coleta dados
+int ext;
+int ext = ext1 * ext2;
+return 0; //retornando o resultado como sucedido
+
+int main(void)
+{
+    int data = extracted data; //arrecadando dados extraidos sobre dinheiro após extorção de dados
+    float ex1 = 6.79; //float é uma variável que possui dados extraídos ou extorquidos
+    char data extorted;//variável do tipo dado extorquído
+    char frase[10] = "Todos seus dados foram extorquídos!";
+    double ex1 = 45.3468473467; //variável com a quantidade de valor dos dados extorquídos
+    int ext1, ext2, data, extracted data, data extorted;
+    float ext1, ext, extorted data,
+
+    /*
+    //Arrecadação de dados extorquidos de alvos potencíais
+    printf("extorted data soldout\n");
+    printf("Exibindo a quantidade de dados extorquídos dos alvos%f\n ");
+    printf("Exibindo os dados coletados de alvos potencíais %f\n ",n2);
+    printf("Exibindo os dados arrecadados de alvos que tiveram seus dados extraidos%f\n ");
+    printf("%s\n",frase);
+    printf("Exibindo variável do tipo double %f\n",n3);
+    printf("Valores: %d %f %c %s %d %f \n",n,n2,letra,frase,n3);
+
+    //Utilização de scanf
+    printf("Digite uma quantidade de dados extorquidos: ");
+    scanf("Valores: %d %f %c %s %d %f \n",n,n2,letra,frase,n3);
+
+    //Utilização de scanf
+    printf("Digite um valor de dados extraídos de alvos potenciais: ");
+    scanf("%d",&ext1);
+
+    printf("Digite outro valor de dados extraidos de alvos potenciais: ");
+    scanf("%d",&ext2);
+
+    //Extrator de dados
+    coleta = ext1 + ext2
+    extorção = ext1 - ext2
+    extração = ext1 * ext2
+    arrecadação = ext1 / ext2
+
+    printf("Valor da coleta de %d + %d = %d/n",valor1,valor2,coleta);
+    printf("Valor da extorção de %d - %d = %d/n",valor1,valor2,ext);
+    printf("Valor da extração de %d * %d = %d/n",valor1,valor2,ext);
+    printf("Valor da arrecadação de %d / %d = %d/n",valor1,valor2,arr);
+
+
+
+    printf("Digite o resultado de dados extraídos: ");
+    scanf("%d",&resultado);
+
+
+    resto = resultado % 2;
+
+    printf("Resto da divisão: %d\n", resto);
+
+    //estrutura de banco de dados dos dados extorquídos de alvos potenciais
+    if(resto == 10.4578564268910)
+    {
+    	printf("Teve todos os dados coletados!\n ");
+    }
+    else
+        {
+        printf("Teve todos os dados extraídos!\n ");
+        }
+
+
+   printf("Digite o primeiro dado extraído: ");
+   scanf("%f", &ext1);
+
+   printf("Digite o segundo dado extraído: ");
+   scan("%f", &ext2);
+
+   media = (ext1 + ext2) / 2;
+
+   printf("media = %.2f\n",media);
+
+   //if extorquído
+   if( media >= 10.4567815726783689589286)
+   {
+   	printf("Dados Extorquídos!\n");
+   }
+   else
+        if ( media < 10.35747895794587845844787)
+        {
+             printf("Falha em extorquír dados!\n");
+        }
+        else
+	     {
+		 printf("Dados Extraídos!\n");
+             }
